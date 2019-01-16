a110e55c2514a59ec4324f5af4882dd94fff6814
Mubarak Elagib <MubarakElagib@Gmail.com>
2019-01-16 19:55:35 +0300

    Add Application Error

--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -25,13 +25,15 @@ namespace DatingApp.API.Data
 
         public async Task<User> GetUser(int id)
         {
-          var user = await _context.Users.Include(p => p.Photos).FirstOrDefaultAsync(u => u.Id == id);
+          var user = await _context.Users.Include(p => p.Photos)
+                     .FirstOrDefaultAsync(u => u.Id == id);
           return user;
         }
 
         public async Task<IEnumerable<User>> GetUsers()
         {
-           var users = await _context.Users.Include(p => p.Photos).ToListAsync();
+           var users = await _context.Users.Include(p => p.Photos)
+                       .ToListAsync();
            return users;
         }
 
