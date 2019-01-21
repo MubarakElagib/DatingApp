5aff3dae8132d9434b7dc3be77b094da22d65d9c
Mubarak Elagib <MubarakElagib@Gmail.com>
2019-01-21 16:23:57 +0300

    End Section 10 part 1

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-nav',
@@ -9,6 +10,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
+  @ViewChild('loginForm') loginForm: NgForm;
   model: any = {};
 
   constructor(public authService: AuthService, private alertify: AlertifyService,
@@ -20,6 +22,7 @@ export class NavComponent implements OnInit {
   login() {
     this.authService.login(this.model).subscribe(next => {
       this.alertify.success('Logged in successfully');
+      this.loginForm.reset();
     }, error => {
       this.alertify.error('Failed to login');
     }, () => {
